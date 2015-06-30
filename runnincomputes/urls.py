dabfd5661042a1e0d2ef8990d9f03253d850ca4a
judgegrubb <elijah.grubb@utah.edu>
2015-06-30 17:14:45 -0600

    updated admin page

--- runnincomputes/urls.py
+++ runnincomputes/urls.py
@@ -19,3 +19,6 @@ from django.contrib import admin
 urlpatterns = [
     url(r'^admin/', include(admin.site.urls)),
 ]
+
+admin.site.site_title = 'Runnin-Computes Admin'
+admin.site.site_header = 'Runnin-Computes Administration'
